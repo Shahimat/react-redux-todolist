fe9dbbeb32b6c564952108d80bf63045237d8139
Shahimat <ngs22071993@gmail.com>
2022-02-14 13:47:47 +0300

    feat[ui]: added list

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import './Content.css';
 
+function ListItem ({ value }) {
+  return (
+    <li>
+      {value}
+    </li>
+  );
+}
+
+function TodoList({ todos }) {
+  const aListItems = todos.map(({ id, value }) =>
+    <ListItem key={`${id}`} value={value} />
+  );
+  return (
+    <ul>
+      {aListItems}
+    </ul>
+  );
+}
+
 function App() {
   const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
   useEffect(() => {
-    console.log(text);
+    console.log(todos);
   });
   return (
     <div className='content'>
@@ -13,11 +33,15 @@ function App() {
         onChange={(oEvent) => setText(oEvent.target.value)} 
         onKeyPress={oEvent => {
           if (oEvent.code === 'Enter') {
+            setTodos((aTodos) => [{
+              id: aTodos.length,
+              value: text,
+            }, ...aTodos]);
             setText('');
           }
         }}
       />
-      <p>{text}</p>
+      <TodoList todos={todos}/>
     </div>
   );
 }
