19947c4224a31cb87205f5d0c522eada3b69d10b
Shahimat <ngs22071993@gmail.com>
2022-02-17 16:44:52 +0300

    refactor[ui]: Content -> Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Layout from './../../layout/index';
 import Header from './../../components/feature/Header/index';
-import Content from './../../components/feature/Content/index';
+import TodoList from './../../components/feature/TodoList/index';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo } from '../../store/Todos';
 
 function Home(props) {
+  const todos = useSelector((state) => state.todos.list);
+  const dispatch = useDispatch();
+  const [text, setText] = useState('');
+
+  const handleInputChange = (oEvent) => setText(oEvent.target.value);
+
+  const handleInputAccept = (oEvent) => {
+    if (oEvent.code === 'Enter') {
+      dispatch(addTodo({ value: text }))
+      setText('');
+    }
+  };
+
   return (
     <Layout>
       <Header title="TodoMVC"/>
-      <Content/>
+      <div className='HomePageContent'>
+        <input
+          className='HomePageContentInput'
+          value={text}
+          onChange={handleInputChange}  
+          onKeyPress={handleInputAccept}
+        />
+        <TodoList todos={todos}/>
+      </div>
     </Layout>
   );
 }
